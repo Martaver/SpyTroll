3064e93da18739ef358641672132aa1053ed9777
Justine De Caires <justine@minerva.kgi.edu>
2016-11-25 22:05:28 +0200

    i borked it

--- server/appBody.js
+++ server/appBody.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import path from "path"
 import async from 'async'
-import { convertToRaw, EditorState } from 'draft-js'
 import mustacheExpress from 'mustache-express'
 
 //////////////////////////////////////////////////////////////////////////////
