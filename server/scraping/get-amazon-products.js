e7acea88bd6de371061bf88a54ea29f6fd529543
Sebastian Nemeth <sebastian@ilixir.fi>
2016-11-26 12:08:01 +0200

    Updated get-amazin-products to read list of related companies from 'Brands' list.
    Working example of a simple scraper filtering elements.

--- server/scraping/get-amazon-products.js
+++ server/scraping/get-amazon-products.js
@@ -1,14 +1,35 @@
 var scraperjs = require('scraperjs');
+var _ = require('lodash');
+//import scraperjs from 'scraperjs'
 
 
 //A simple scraper,
 
-scraperjs.StaticScraper.create('https://www.amazon.com/s/ref=sr_nr_p_89_5?fst=as%3Aoff&rh=i%3Aaps%2Ck%3Ae-reader%2Cp_89%3ASony&keywords=e-reader&ie=UTF8&qid=1480116541&rnid=2528832011')
-	.scrape(function($) {
-		return $("a h2").map(function() {
-			return $(this).text();
-		}).get();
-	})
-	.then(function(news) {
-		console.log(news);
-	})
+scraperjs.StaticScraper.create('https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=ereader')
+
+.scrape(function($) {
+	var ulBrands = $("h2")
+		//Find the h2 element with text 'Brand'
+		.filter(function() { return $(this).text() == 'Brand'})
+
+		//Select the next element, it will be the UL, list of brands (Companies).
+		.map(function() { return $(this).next();})
+
+		//Get the first element, it will be the single UL tag.
+		.get()[0];
+
+	return $(ulBrands)
+
+		//Get the span in each li.
+		.find('li a span')
+
+		//Get the text element of each span, which will give us our list of brands.
+		.map(function() { return $(this).text() });
+
+})
+
+.then(function(news) {
+
+	//Save the company names here.
+	console.log(news);
+})
