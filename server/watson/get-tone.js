ec4db72615d6189c496f4a7130c3cab29ddb42e5
Sebastian Nemeth <sebastian@ilixir.fi>
2016-11-26 21:30:33 +0200

    Updating get-tone - this should work, but it doesn't seem to on my machine - Jyrki?

---
+++ server/watson/get-tone.js
@@ -0,0 +1,36 @@
+var _ = require('lodash');
+var request = require('request');
+
+// var unirest = require('unirest');
+
+function getTone(text) {
+
+	console.log('doing stuff...');
+
+	// request('http://www.google.com', function (error, response, body) {
+	// 	if (!error && response.statusCode == 200) {
+	// 		console.log(body) // Show the HTML for the Google homepage.
+	// 	}
+	// })
+
+	var data = { 'text': text };
+	request({
+		url: 'http://spytroll.eu-gb.mybluemix.net/analyze-text',
+		method: 'POST',
+		//Lets post the following key/values as form
+		json: data
+	}, function(error, response, body){
+
+		if(error) { console.log(error); }
+		else { console.log(response.statusCode, body); }
+	});
+
+	// unirest.post('http://spytroll.eu-gb.mybluemix.net/analyze-text')
+	// .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+	// .send({ "text": "The fat black cat sat in the hat and shat." })
+	// .end(function (response) {
+	// 	console.log(response.body);
+	// });
+}
+
+getTone('The fat black cat sat in the hat and shat.');
