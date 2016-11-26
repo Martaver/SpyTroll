89f44a4cf972d0bf9cca15c41ff3d62856786d5f
Sebastian Nemeth <sebastian@ilixir.fi>
2016-11-26 22:13:29 +0200

    REQUESTS TO WATSON BIATCH! AWWW YIIIIIII!

--- server/watson/get-tone.js
+++ server/watson/get-tone.js
@@ -1,37 +1,21 @@
 var _ = require('lodash');
-var req = require('request');
-
-// var unirest = require('unirest');
+var request = require('request');
 
 function getTone(text) {
 
 	console.log('doing stuff...');
 
-	// request('http://www.google.com', function (error, response, body) {
-	// 	if (!error && response.statusCode == 200) {
-	// 		console.log(body) // Show the HTML for the Google homepage.
-	// 	}
-	// })
+	var data = { text: text };
 
-	var data = { 'text': text };
-	req({
-		// url: 'http://spytroll.eu-gb.mybluemix.net/analyze-text',
-		url: 'http://spytroll.eu-gb.mybluemix.net',
-		method: 'POST',
-		//Lets post the following key/values as form
-		json: data
+	request.post({
+		uri: 'http://spytroll.eu-gb.mybluemix.net/analyze-text',
+		body: JSON.stringify(data)
 	}, function(error, response, body){
 
 		if(error) { console.log(error); }
 		else { console.log(response.statusCode, body); }
 	});
 
-	// unirest.post('http://spytroll.eu-gb.mybluemix.net/analyze-text')
-	// .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-	// .send({ "text": "The fat black cat sat in the hat and shat." })
-	// .end(function (response) {
-	// 	console.log(response.body);
-	// });
 }
 
 getTone('The fat black cat sat in the hat and shat.');
