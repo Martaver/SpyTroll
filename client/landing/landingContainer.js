7f56fb8bfdc120238977495fe1213ef50ea5aac5
Justine De Caires <justine@minerva.kgi.edu>
2016-11-26 18:18:31 +0200

    I dont feel like writing a real commit message...

--- client/landing/landingContainer.js
+++ client/landing/landingContainer.js
@@ -4,25 +4,13 @@ import Landing from './landing'
 import { changeFieldValue } from '../form/formActions'
 
 const mapStateToProps = (state) => {
-    let termsForm = state.getIn(['form', 'terms'])
-    let terms = state.get('terms')
     return {
-        companies: state.get('entities').get('companies').toJS(),
-        terms: termsForm ? termsForm.get('terms').toJS(): { value: '' }
+        companies: state.get('entities').get('companies').toJS()
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        handleChange: (e) => {
-            dispatch(changeFieldValue(e.target.name, e.target.value))
-        },
-        handleSubmit: (e) => {
-            e.preventDefault()
-            // dispatch(createNewSearch())
-            return false
-        }
-    }
+    return {}
 }
 
 const LandingContainer = connect(mapStateToProps, mapDispatchToProps)(Landing)
---
+++ client/termInput/termInputReducer.js
@@ -0,0 +1,9 @@
+import Immutable from 'immutable'
+
+const initialState = Immutable.Map()
+
+function termInputReducer(state=initialState, action) {
+    return state
+}
+
+export default termInputReducer
