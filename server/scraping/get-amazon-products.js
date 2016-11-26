94f02fb96c660dc8473f5bed9430aad0803933da
Sebastian Nemeth <sebastian@ilixir.fi>
2016-11-26 10:18:56 +0200

    Simple static scraper for products from Amazon.

--- server/scraping/get-amazon-products.js
+++ server/scraping/get-amazon-products.js
@@ -1,7 +1,11 @@
 var scraperjs = require('scraperjs');
-scraperjs.StaticScraper.create('https://news.ycombinator.com/')
+
+
+//A simple scraper,
+
+scraperjs.StaticScraper.create('https://www.amazon.com/s/ref=sr_nr_p_89_5?fst=as%3Aoff&rh=i%3Aaps%2Ck%3Ae-reader%2Cp_89%3ASony&keywords=e-reader&ie=UTF8&qid=1480116541&rnid=2528832011')
 	.scrape(function($) {
-		return $(".title a").map(function() {
+		return $("a h2").map(function() {
 			return $(this).text();
 		}).get();
 	})
