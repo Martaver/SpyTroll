9410d9de563bcc1b2e2d109d55c6ecb0ec9db75d
Sebastian Nemeth <sebastian@ilixir.fi>
2016-11-26 13:29:23 +0200

    Added amazon scraper, call 'scrapeAmazon' and provide newCompany and newProduct callbacks.

--- server/scraping/get-amazon-products.js
+++ server/scraping/get-amazon-products.js
@@ -1,35 +1,91 @@
-var scraperjs = require('scraperjs');
-var _ = require('lodash');
-//import scraperjs from 'scraperjs'
+// var scraperjs = require('scraperjs');
+// var _ = require('lodash');
+// var uuid = require('uuid');
+import scraperjs from 'scraperjs'
+import _ from 'lodash'
+import uuid from 'uuid'
 
+function getBrandUrlFromSearch(search) {
+	return 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords='+search;
+}
 
-//A simple scraper,
+function getProductUrlFromBrandAndSearch (brand, search) {
 
-scraperjs.StaticScraper.create('https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=ereader')
+	return 'https://www.amazon.com/gp/search/ref=sr_nr_p_89_5?fst=as%3Aoff&rh=i%3Aaps%2Ck%3Ae-reader%2Cp_89%3A'+brand+'&keywords='+search+'&ie=UTF8&qid=1480151129&rnid=2528832011'
+}
 
-.scrape(function($) {
-	var ulBrands = $("h2")
-		//Find the h2 element with text 'Brand'
-		.filter(function() { return $(this).text() == 'Brand'})
+export default function scrapeAmazon(search, newCompanyCallback, newProductCallback) {
+// function scrapeAmazon(search, newCompanyCallback, newProductCallback) {
 
-		//Select the next element, it will be the UL, list of brands (Companies).
-		.map(function() { return $(this).next();})
+		function scrapeBrands($) {
 
-		//Get the first element, it will be the single UL tag.
-		.get()[0];
+			var ulBrands = $("h2")
 
-	return $(ulBrands)
+			//Find the h2 element with text 'Brand'
+				.filter(function() { return $(this).text() == 'Brand'})
 
-		//Get the span in each li.
-		.find('li a span')
+				//Select the next element, it will be the UL, list of brands (Companies).
+				.map(function() { return $(this).next();})
 
-		//Get the text element of each span, which will give us our list of brands.
-		.map(function() { return $(this).text() });
+				//Get the first element, it will be the single UL tag.
+				.get()[0];
 
-})
+			return $(ulBrands)
 
-.then(function(news) {
+			//Get the span in each li.
+				.find('li a span')
 
-	//Save the company names here.
-	console.log(news);
-})
+				//Get the text element of each span, which will give us our list of brands.
+				.map(function() { return $(this).text() })
+		}
+
+		function onScrapeBrandsComplete(brands) {
+
+			_.each(brands, function(brand){
+
+				// For each brand, look up the products that that brand owns.
+				var company = {
+					id: uuid(),
+					name: brand
+				};
+
+				newCompanyCallback(company);
+
+				return scraperjs.StaticScraper
+
+				//Create url request for the brand/search term.
+					.create(getProductUrlFromBrandAndSearch(brand, search))
+
+					//Find the h2 element of each item, and get each's text.
+					.scrape(function($) {
+
+						return $("div.s-item-container h2").map(function() { return $(this).text() })
+					})
+
+					.then(function(productNames) {
+
+						_.each(productNames, function(productName) {
+
+							var product = {
+								id: uuid(),
+								name: productName,
+								companyId: company.id
+							};
+
+							newProductCallback(product);
+						})
+					});
+			})
+		}
+
+		return scraperjs.StaticScraper.create(getBrandUrlFromSearch(search))
+
+			.scrape(scrapeBrands)
+			.then(onScrapeBrandsComplete)
+}
+
+// scrapeAmazon('e-reader', function(company) {
+// 	console.log('Company: '+JSON.stringify(company));
+// }, function(product) {
+// 	console.log('Product: '+JSON.stringify(product));
+// });
